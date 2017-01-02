77a8760687edbbb0f9700ff14a23ead3760f6eb2
Banbury <banbury20@gmail.com>
2017-01-02 12:38:14 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Import image as mesh
+
+This is an add-on for [Blender](http://www.blender.org), that imports images as meshes. If the image contains alpha
+transparency, the mesh will have the shape of the opaque parts of the image. The resulting image is scaled according
+to its resolution in inches.
+
+## Installation
+
+Download the repository as Zip file. Then install the add-on in Blender with
+File/User Preferences/Add-ons/Install from file.
+
+## Usage
+
+Import an image with File/Import/Image As Mesh.
+
+Currently only PNGs are allowed. Images without alpha information (like JPGs) can be imported with
+File/Import/Images as Planes.
+
+## Implementation details
+
+The add-on uses the Marching Square algorithm to create a polygon around the non-transparent parts of the imported
+image.
+Then the polygon is reduced with the Ramer-Douglas-Peucker algorithm.
+Blender's bmesh.ops.triangle_fill is used to create the mesh from the polygon.
+Finally uv coordintes are calculated, a material is created and the mesh is scaled to the size of the image.
+
+## License information
+
+The [RDP library](https://github.com/sebleier/RDP) licensed under the BSD.
+
+Everything else is GPL licensed.
---
+++ __init__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import bpy
+from bpy_extras.io_utils import ImportHelper
+from io_image_as_mesh.image_as_mesh import create_mesh_from_image
+
+bl_info = {
+    "name": "Image as Mesh Importer",
+    "description": "Import Image as Mesh.",
+    "author": "Banbury",
+    "version": (1, 0, 0),
+    "blender": (2, 78, 0),
+    "location": "File > Import",
+    "wiki_url": "",
+    "category": "Import-Export"}
+
+class ImageAsMeshOps(bpy.types.Operator, ImportHelper):
+    """LDR Importer Operator"""
+    bl_idname = "import_scene.image_as_mesh"
+    bl_description = "Import an image as a mesh."
+    bl_label = "Import Image as Mesh"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_options = {'REGISTER', 'UNDO', 'PRESET'}
+
+    # File type filter in file browser
+    filename_ext = ".png"
+
+    filter_glob = bpy.props.StringProperty(
+        default="*.png",
+        options={'HIDDEN'}
+    )
+
+    def execute(self, context):
+        path = self.filepath
+        img = bpy.data.images.load(path, check_existing=True)
+        create_mesh_from_image(img)
+
+        return {'FINISHED'}
+
+def menu_import(self, context):
+    """Import menu listing label"""
+    self.layout.operator(ImageAsMeshOps.bl_idname, text="Image as Mesh (.png)")
+
+def register():
+    """Register Menu Listing"""
+    bpy.utils.register_module(__package__)
+    bpy.types.INFO_MT_file_import.append(menu_import)
+
+
+def unregister():
+    """Unregister Menu Listing"""
+    bpy.utils.unregister_module(__package__)
+    bpy.types.INFO_MT_file_import.remove(menu_import)
+
+if __name__ == "__main__":
+    register()
