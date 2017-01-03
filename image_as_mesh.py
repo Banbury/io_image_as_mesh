346ab3dbae636ab4a1d83911edbd64fbcd5b02dc
Banbury <banbury20@gmail.com>
2017-01-03 16:45:54 +0100

    Borrowed triangulation algorithm from CoaTools.

--- image_as_mesh.py
+++ image_as_mesh.py
@@ -72,6 +72,19 @@ def create_sprite(poly, img):
     bm.verts.index_update()
 
     triangle_fill = bmesh.ops.triangle_fill(bm, edges=bm.edges[:], use_dissolve=False, use_beauty=True)
+    if triangle_fill:
+        average_edge_cuts(bm,obj)
+        triangulate(bm,obj)
+        smooth_verts(bm,obj)
+        collapse_short_edges(bm,obj)
+        smooth_verts(bm,obj)
+        clean_verts(bm,obj)
+        smooth_verts(bm,obj)
+        triangulate(bm,obj)
+        smooth_verts(bm,obj)
+
+        if hasattr(bm.verts, "ensure_lookup_table"):
+            bm.verts.ensure_lookup_table()
 
     uvtex = bm.faces.layers.tex.new("UVMap")
     uv_lay = bm.loops.layers.uv.new("UVMap")
@@ -81,7 +94,7 @@ def create_sprite(poly, img):
         for loop in face.loops:
             uv = loop[uv_lay].uv
             index = loop.vert.index
-            v = obj.data.vertices[index].co
+            v = bm.verts[index].co
             uv.x = v.x + 0.5
             uv.y = v.z + 0.5
 
@@ -146,4 +159,103 @@ def create_cycles_material(mat, img):
     links.new(tex.outputs[0], diff.inputs[0])
     links.new(diff.outputs[0], out.inputs[0])
 
+# functions borrowed from CoaTools
+def average_edge_cuts(bm,obj,cuts=1):
+    ### collapse short edges
+    edges_len_average = 0
+    edges_count = 0
+    shortest_edge = 10000
+    for edge in bm.edges:
+        if True:#edge.is_boundary:
+            edges_count += 1
+            length = edge.calc_length()
+            edges_len_average += length
+            if length < shortest_edge:
+                shortest_edge = length
+    edges_len_average = edges_len_average/edges_count
 
+    subdivide_edges = []
+    for edge in bm.edges:
+        cut_count = int(edge.calc_length()/shortest_edge)*cuts
+        if cut_count < 0:
+            cut_count = 0
+        if not edge.is_boundary:
+            subdivide_edges.append([edge,cut_count])
+    for edge in subdivide_edges:
+        bmesh.ops.subdivide_edges(bm,edges=[edge[0]],cuts=edge[1])
+        bmesh.update_edit_mesh(obj.data)
+
+def triangulate(bm,obj):
+    bmesh.ops.triangulate(bm,faces=bm.faces)
+    bmesh.update_edit_mesh(obj.data)
+
+def smooth_verts(bm,obj):
+    ### smooth verts
+    smooth_verts = []
+    for vert in bm.verts:
+        if not vert.is_boundary:
+            smooth_verts.append(vert)
+    for i in range(50):
+        #bmesh.ops.smooth_vert(bm,verts=smooth_verts,factor=1.0,use_axis_x=True,use_axis_y=True,use_axis_z=True)
+        bmesh.ops.smooth_vert(bm,verts=smooth_verts,factor=1.0,use_axis_x=True,use_axis_y=True,use_axis_z=True)
+    bmesh.update_edit_mesh(obj.data)
+
+def clean_verts(bm,obj):
+    ### find corrupted faces
+    faces = []
+    for face in bm.faces:
+        i = 0
+        for edge in face.edges:
+            if not edge.is_manifold:
+                i += 1
+            if i == len(face.edges):
+                faces.append(face)
+    bmesh.ops.delete(bm,geom=faces,context=5)
+
+    edges = []
+    for face in bm.faces:
+        i = 0
+        for vert in face.verts:
+            if not vert.is_manifold and not vert.is_boundary:
+                i+=1
+            if i == len(face.verts):
+                for edge in face.edges:
+                    if edge not in edges:
+                        edges.append(edge)
+    bmesh.ops.collapse(bm,edges=edges)
+
+    bmesh.update_edit_mesh(obj.data)
+    for vert in bm.verts:
+        if not vert.is_boundary:
+            vert.select = False
+
+    verts = []
+    for vert in bm.verts:
+        if len(vert.link_edges) in [3,4] and not vert.is_boundary:
+            verts.append(vert)
+    bmesh.ops.dissolve_verts(bm,verts=verts)
+    bmesh.update_edit_mesh(obj.data)
+
+def collapse_short_edges(bm,obj,threshold=1):
+    ### collapse short edges
+    edges_len_average = 0
+    edges_count = 0
+    shortest_edge = 10000
+    for edge in bm.edges:
+        if True:
+            edges_count += 1
+            length = edge.calc_length()
+            edges_len_average += length
+            if length < shortest_edge:
+                shortest_edge = length
+    edges_len_average = edges_len_average/edges_count
+
+    verts = []
+    for vert in bm.verts:
+        if not vert.is_boundary:
+            verts.append(vert)
+    bmesh.update_edit_mesh(obj.data)
+
+    bmesh.ops.remove_doubles(bm,verts=verts,dist=edges_len_average*threshold)
+
+    bmesh.update_edit_mesh(obj.data)
